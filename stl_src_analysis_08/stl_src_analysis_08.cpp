b6bfc5bbb94b87208fca331f1a4c41864987f0b9
Yyyyshen <Yyyyshen@gmail.com>
2021-04-07 16:13:13 +0800

    适配器的概念和分类

--- stl_src_analysis_08/stl_src_analysis_08.cpp
+++ stl_src_analysis_08/stl_src_analysis_08.cpp
@@ -6,9 +6,23 @@
 /**
  * 适配器
  */
+using namespace std;
+/**
+ * 概念和分类
+ * 应用于容器
+ *	前面看过的queue和stack就是基于deque的容器适配器
+ * 应用于仿函数
+ *	由于仿函数是operator()操作符的重载，而对于一般函数或者成员函数要想无缝与其他适配器或算法结合，stl提供了很多仿函数适配器
+ * 应用于迭代器
+ *	insert iterators，将一般迭代器的赋值操作转为插入操作
+ *	reverse iterators，将一般迭代器的行进方向逆转，使原本前进的++变成后退操作，反之亦然
+ *	iostream iterators，将迭代器绑定到iostream对象上。例如绑定std::cout，方便输出
+ */
+void test_adapter() {
+
+}
 
 int main()
 {
-    std::cout << "Hello World!\n";
+	std::cout << "Hello World!\n";
 }
-
